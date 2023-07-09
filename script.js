3c6d25d445b89512d3b75ac969a6c66d4559f113
yuriecheverri <leissyesanchez@gmail.com>
2023-07-09 18:30:38 -0500

    Funcionalidad de imagen

--- script.js
+++ script.js
@@ -1,63 +1,116 @@
-function clickNavImg() {
-    const asideText = document.getElementById('asideText');
-    const asideImg = document.getElementById('asideImg');
-    asideImg.style.display = 'inline-flex';
-    asideText.style.display = 'none';
-}
+//Alternar entre paneles
 
-function clickNavText() {
-    const asideText = document.getElementById('asideText');
-    const asideImg = document.getElementById('asideImg');
+const asideText = document.getElementById('textAside');
+const asideImg = document.getElementById('imageAside');
+const navImageButton = document.getElementById('nav-image-button');
+const navTextButton = document.getElementById('nav-text-button');
+const navModeButton = document.getElementById('nav-mode-button');
+const headerElement = document.getElementById('header')
+const mainContainer = document.getElementById('main-container')
+asideText.style.display = 'none';
+
+navTextButton.addEventListener('click', () => {
     asideImg.style.display = 'none';
     asideText.style.display = 'inline-flex';
-}
-console.log(clickNavText)
+})
 
-//function clickNavChangeMode() {
-//  console.log("Cargar tema oscuro");
-//}
+navImageButton.addEventListener('click', () => {
+    asideImg.style.display = 'inline-flex';
+    asideText.style.display = 'none';
+})
 
-function loadImage(){
-    const url = document.getElementById('input-url').value;
-    const image = new Image();
-    image.src = url;
-    const imageDiv = document.getElementById('imageDiv');
-    imageDiv.innerHTML = '';
-    imageDiv.appendChild(image);
-}
+//Modo claro y modo oscuro
 
-//
+navModeButton.addEventListener('click', () => {
+    headerElement.classList.toggle("header-light")
+    mainContainer.classList.toggle("main-container-light")
+    asideText.classList.toggle("aside-light")
+    asideImg.classList.toggle("aside-light")
 
-const colorPicker = document.getElementById('color-picker')
-console.log(colorPicker)
-const colorName = document.getElementById('color-name')
-console.log(colorName)
+    if (mainContainer.classList.contains("main-container-light")) {
+        navModeButton.innerText = "Modo Oscuro";
+    } else {
+        navModeButton.innerText = "Modo Claro";
+    }
+})
 
-const cambiarFondoMeme = () => {
-    console.log(colorPicker.value)
-    colorName.innerHTML = colorPicker.value;
-}   
+//Agregar imagen con url
 
-colorPicker.addEventListener('input',() => cambiarFondoMeme())
-
-//const clicNavChangeMode = () => {
-//  textAside.classlist.toggle('dark-aside')
-//  imgAside.classList.toggle("dark-aside");
-//  mainContainer.classList.toggle("main-container-dark");
-//}
-
-//in (clickNavChangeMode.innerText === "Modo Oscuro"){
-//    clickNavChangeMode.innerHTML = "Modo claro"
-//} else { 
-//  clickNavChangeMode = "Modo oscuro"
-//}
-
-const inputUrl =document.getElementById('input-url')
+const inputUrl = document.getElementById('input-url')
 const memeImg = document.getElementById('imageDiv')
 
 inputUrl.addEventListener('input', (e) => changeBackground(e))
 
 const changeBackground = (e) => {
-    console.log(e.target.value)
-    imageDiv.style.backgroundimage = `url$(e.target.value)`
+    memeImg.style.backgroundImage = `url('${e.target.value}')`
+    memeImg.style.backgroundSize = 'cover'
+    memeImg.style.backgroundRepeat = 'no-repeat'
 }
+
+//Cambiar color de fondo
+
+const colorPicker = document.getElementById('color-picker');
+const colorName = document.getElementById('color-name');
+
+colorPicker.addEventListener('input', () => cambiarFondoMeme());
+
+const cambiarFondoMeme = () => {
+    colorName.innerHTML = colorPicker.value;
+    memeImg.style.backgroundColor = colorPicker.value;
+}
+
+//Cambiar modo de mezcla de fondo
+
+const selectChangeImg = document.getElementById('image-option')
+
+selectChangeImg.addEventListener('input', (e) => {
+    memeImg.style.backgroundBlendMode = e.target.value;
+})
+
+// Filtros
+
+const inputBrightness = document.getElementById('brightness');
+const inputOpacity = document.getElementById('opacity');
+const inputContrast = document.getElementById('contrast')
+const inputBlur = document.getElementById('blur')
+const inputGrayscale = document.getElementById('grayscale')
+const inputSepia = document.getElementById('sepia')
+const inputHueRotation = document.getElementById('hue-rotation')
+const inputSaturation = document.getElementById('saturation')
+const inputInvert = document.getElementById('invert')
+
+inputBrightness.addEventListener('input', () => changeValueFilters());
+inputOpacity.addEventListener('input', () => changeValueFilters());
+inputContrast.addEventListener('input', () => changeValueFilters());
+inputBlur.addEventListener('input', () => changeValueFilters());
+inputGrayscale.addEventListener('input', () => changeValueFilters());
+inputSepia.addEventListener('input', () => changeValueFilters());
+inputHueRotation.addEventListener('input', () => changeValueFilters());
+inputSaturation.addEventListener('input', () => changeValueFilters());
+inputInvert.addEventListener('input', () => changeValueFilters());
+
+const changeValueFilters = () => {
+    memeImg.style.filter = `brightness(${inputBrightness.value}) opacity(${inputOpacity.value}) blur(${inputBlur.value}) contrast(${inputContrast.value}%) grayscale(${inputGrayscale.value}%) hue-rotate(${inputHueRotation.value}) sepia(${inputSepia.value}%) saturate(${inputSaturation.value}%) invert(${inputInvert.value})`
+
+}
+
+inputBrightness.value = 0;
+
+
+//Abrir y cerrar panel
+//Descargar imagen
+//Restablecer filtros
+
+//Texto
+//Editar texto superior
+//Remover texto superior
+//Editar texto inferior
+//Remover texto inferior
+//Cambiar familia de fuente
+//Cambiar tamaño de fuente
+//Cambiar alineación
+//Cambiar color de texto
+//Cambiar color de fondo de texto
+//Fondo transparente
+//Cambiar padding de texto
+//Cambiar alto de línea de texto
